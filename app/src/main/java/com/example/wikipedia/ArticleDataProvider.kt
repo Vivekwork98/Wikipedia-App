5a765c028e9d9b8f8fb43989d73f6aee9afc9c2e
Vivekwork98 <vivekaadhya@gmail.com>
2019-05-30 12:42:30 +0530

    Commit

--- app/src/main/java/com/example/wikipedia/ArticleDataProvider.kt
+++ app/src/main/java/com/example/wikipedia/ArticleDataProvider.kt
@@ -8,53 +8,44 @@ import com.google.gson.Gson
 import java.io.Reader
 
 
-class ArticleDataProvider
-{
+class ArticleDataProvider {
     init {
-        FuelManager.instance.baseHeaders = mapOf("User-Agent" to "User" )
+        FuelManager.instance.baseHeaders = mapOf("User-Agent" to "User")
     }
-    fun search(term:String , skip:Int , take:Int , responseHandler: (result:WikiResult) -> Unit? )
-    {
-        Urls.getSearchUrl(term,skip, take).httpGet()
-            .responseObject(WikipediaDataDeserializer()){ _, response,result->
-                if(response.httpStatusCode != 200)
-                {
+
+    fun search(term: String, skip: Int, take: Int, responseHandler: (result: WikiResult) -> Unit?) {
+        Urls.getSearchUrl(term, skip, take).httpGet()
+            .responseObject(WikipediaDataDeserializer()) { _, response, result ->
+                if (response.httpStatusCode != 200) {
                     throw Exception("Unable to get articles")
                 }
-                val(data, _) = result
-                ((data as? WikiResult?).let {
-                    if (it != null) {
-                        responseHandler.invoke(it)
-                    }
-                })
-                Log.d("gddx","my")
+                val (data, _) = result
+                Log.d("gddx", "my")
+                responseHandler.invoke(data as WikiResult)
+            }
 
-        }
+
     }
 
-    fun getRandom(take:Int, responseHandler: (result: WikiResult) -> Unit?)
-    {
+    fun getRandom(take: Int, responseHandler: (result: WikiResult) -> Unit?) {
 
         Urls.getRandomUrl(take).httpGet()
-            .responseObject(WikipediaDataDeserializer()){ _, response,result->
+            .responseObject(WikipediaDataDeserializer()) { _, response, result ->
                 //response.httpStatusCode = 200
-                if(response.httpStatusCode != 200)
-                {
+                if (response.httpStatusCode != 200) {
                     throw Exception("Unable to get articles")
                 }
-                val(data, _) = result
-                ((data as? WikiResult?).let {
-                    if (it != null) {
-                        responseHandler.invoke(it)
-                    }
-                })
-                Log.d("gddx","fcg")
-
+                val (data, _) = result
+                Log.d("gddx", "fcg")
+                responseHandler.invoke(data as WikiResult)
             }
-    }
-    class WikipediaDataDeserializer : ResponseDeserializable<WikiResult>
-    {
-        override fun deserialize(reader: Reader) = Gson().fromJson(reader,WikiResult::class.java)
-    }
-}
 
+
+    }
+
+    
+    class WikipediaDataDeserializer : ResponseDeserializable<WikiResult> {
+        override fun deserialize(reader: Reader) = Gson().fromJson(reader, WikiResult::class.java)
+    }
+
+}
